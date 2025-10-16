7358ea40f50e7a5057004e893b624b8cefa0b0ae
Yash Gupta <120112432+Yashgupta9132@users.noreply.github.com>
2025-10-16 14:29:22 +0530

    Create Remove BST keys outside given range.java

---
+++ Remove BST keys outside given range.java
@@ -0,0 +1,16 @@
+// Java 
+
+class Solution {
+    Node removekeys(Node root, int l, int r) {
+        if (root == null)
+            return null;
+        if (root.data < l)
+            return removekeys(root.right, l, r);
+        if (root.data > r)
+            return removekeys(root.left, l, r);
+        root.left = removekeys(root.left, l, r);
+        root.right = removekeys(root.right, l, r);
+
+        return root;
+    }
+}
